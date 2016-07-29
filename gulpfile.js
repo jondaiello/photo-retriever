a81f787ee1f7e3bc711b23ef686d743e0b974da6
Jon Daiello <jondaiello@gmail.com>
2016-07-28 21:27:28 -0400

    setting up gulp

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+
+gulp.task('js', function() {
+  gulp.src('./src/js/*.js')
+    .pipe(sourcemaps.init())
+      .pipe(concat('all.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('./src/js/*.js')
+    .pipe(gulp.dest('./js'));
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('./css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./src/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/sass/*.scss', ['sass']);
+});
