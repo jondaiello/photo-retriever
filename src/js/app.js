e36b72551b3ab9057cc73a85d7f507eb98785d26
Jon Daiello <jondaiello@gmail.com>
2016-07-28 21:57:17 -0400

    building the initial API request

---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+/* This is where we will get our photos from. */
+const API_URL = "https://api.unsplash.com/photos/?client_id=5c680ed0727325d7218e6fcbf3a5fbeac540121b2fe88064fdb54e9011dc7aef&per_page=9&order_by=popular";
+
+
+/* This function will return the photos from our source defined above */
+function getPhotos() {
+  var photos = new XMLHttpRequest();
+
+  photos.onreadystatechange = function() {
+    if ( photos.readyState == 4 && photos.status == 200 ) {
+      document.getElementById('photo-grid').innerHTML = photos.responseText;
+    }
+  };
+
+  photos.open("GET", "https://api.unsplash.com/photos/?client_id=5c680ed0727325d7218e6fcbf3a5fbeac540121b2fe88064fdb54e9011dc7aef&per_page=9", true);
+  photos.send();
+}
+
+function init() {
+  getPhotos();
+}
+
+window.onload = init();
