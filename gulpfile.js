d186ab54753e8fe6cc4763940027cade1ec17e9e
Jon Daiello <jondaiello@gmail.com>
2016-07-29 13:24:08 -0400

    adding a gulp js:watch task

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,10 @@ gulp.task('js', function() {
     .pipe(gulp.dest('./js'));
 });
 
+gulp.task('js:watch', function () {
+  gulp.watch('./src/js/*.js', ['js']);
+});
+
 gulp.task('scripts', function() {
   return gulp.src('./src/js/*.js')
     .pipe(gulp.dest('./js'));
