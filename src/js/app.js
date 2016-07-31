585330a45a3588a64aa9edf27d137f533630759c
Jon Daiello <jondaiello@gmail.com>
2016-07-30 21:45:40 -0400

    Adding data-img-id attribute to image links

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,15 @@
 /* This is where we will get our photos from. */
 const API_URL = "https://api.unsplash.com/photos/?client_id=5c680ed0727325d7218e6fcbf3a5fbeac540121b2fe88064fdb54e9011dc7aef&per_page=9&order_by=popular";
 
-function buildPreview(imgURL) {
+function buildPreview(imgURL, imgID) {
   var a = document.createElement('a');
   a.href = imgURL;
   a.target = "_blank";
   a.className = "c-preview";
+  a.setAttribute('data-img-id', imgID);
   var img = document.createElement('img');
   img.src = imgURL;
+  img.className = "c-preview__img";
   a.appendChild(img);
   document.getElementById('photo-grid').appendChild(a);
 }
@@ -20,7 +22,7 @@ function getPhotos() {
     if ( photos.readyState == 4 && photos.status == 200 ) {
       var photoData = JSON.parse(photos.responseText);
       photoData.forEach(function(e, i) {
-        buildPreview(e.urls.small);
+        buildPreview(e.urls.small, e.id);
       });
     }
   };
@@ -29,8 +31,9 @@ function getPhotos() {
   photos.send();
 }
 
+var previews = document.querySelectorAll('.c-preview');
+
 function init() {
-  //document.getElementById('photo-grid').innerHTML =
   getPhotos();
 }
 
