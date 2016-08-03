416a56f3c0db753c899e3400fd3f58d0fbb827ca
Jon Daiello <jondaiello@gmail.com>
2016-08-02 22:47:22 -0400

    turning on js minify and removing unused tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,31 +2,28 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var cleanCSS = require('gulp-clean-css');
+var minify = require('gulp-minify');
 var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 
 gulp.task('js', function() {
   gulp.src('./src/js/*.js')
-    //.pipe(sourcemaps.init())
-      .pipe(concat('all.js'))
-    //.pipe(sourcemaps.write())
+    .pipe(sourcemaps.init())
+    .pipe(concat('all.js'))
+    .pipe(
+      minify({
+        ext:{
+            src:'-debug.js',
+            min:'.js'
+        },
+    })
+    )
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./js'));
 });
 
 gulp.task('js:watch', function () {
   gulp.watch('./src/js/*.js', ['js']);
-});
-
-gulp.task('scripts', function() {
-  return gulp.src('./src/js/*.js')
-    .pipe(gulp.dest('./js'));
-});
-
-gulp.task('minify-css', function() {
-  return gulp.src('./css/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./css'));
 });
 
 gulp.task('sass', function () {
